39e600dd439a35428ff5cf6b8b881bb4658929ec
James Li <james@onfleet.com>
2020-08-19 12:14:26 -0700

    Convert config JSON to Python module

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / "README.md").read_text()
 
 setuptools.setup(
     name="pyonfleet",  
-    version= "1.0.3.4",
+    version= "1.1.0",
     author="James Li",
     author_email="support@onfleet.com",
     description="Onfleet's Python API Wrapper Package",
@@ -14,10 +14,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="http://docs.onfleet.com",
     packages=setuptools.find_packages(),
-    data_files=[
-        ("config", ["config/config.json"])
-    ],
     include_package_data=True,
+    package_data={
+        "": ["*.json"]
+    },
     install_requires=[
         "requests", "configparser", "ratelimit", "backoff", "pathlib"
     ],
