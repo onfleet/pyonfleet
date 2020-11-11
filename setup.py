c9ff116981222a7d95df4dfd94f1ddb79d928427
James Li <james@onfleet.com>
2020-11-10 16:45:09 -0800

    Update to versioning due to new wheel distribution

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / "README.md").read_text()
 
 setuptools.setup(
     name="pyonfleet",  
-    version= "1.1.0",
+    version= "1.1.1",
     author="James Li",
     author_email="support@onfleet.com",
     description="Onfleet's Python API Wrapper Package",
